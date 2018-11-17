f5e586a2c26ffecc2256d61d3a6caf566c8463dd
Joe Flanagan <josephbflanagan@gmail.com>
2018-11-17 10:09:37 -0500

    Added handleLine() function to determine what should be done with stdin line

--- servermon.py
+++ servermon.py
@@ -15,6 +15,38 @@ class Connection:
 		self.connect_time = connect_time
 		self.disconnect_time = disconnect_time
 
+
+def handleLine(line):
+	"""
+	Decides what to do with incoming data based on words in line,
+	connected = create connection object
+	disconnected = destroy connection object, save to log file
+	killed = display line, update kills and deaths for connection objects
+	joined = display team change line, update server / team data
+	"""
+	action_w = ""
+	find_words = ['connected','disconnected','joined','killed']
+	connected = re.compile(r'\sconnected')
+
+
+
+	if ' connected' in line:
+		print('Handling Connection Line')
+		print(line)
+	elif 'disconnected' in line:
+		print('Handling Disconnection Line')
+		print(line)
+	elif 'joined' in line:
+		print('Handling Joined Line')
+		print(line)
+	elif 'killed' in line:
+		print('Handling Killed Line')
+		print(line)
+	else:
+		print('No Action Words found in line.')
+		print(line)
+
+
 def parseInfo(line):
 	"""
 	Parses incoming strings from DOI Source Server, decides if the
@@ -24,13 +56,15 @@ def parseInfo(line):
 	"""
 	date = ""
 	time = ""
-	name = ""
+	connect_time = ""
+	disconnect_time = ""
+	uname = ""
+	steamID = ""
+
 	"""
 	Set patterns to search for
 	date: mm/dd/yyyy
 	time: hr:mm:ss
-
-
 	"""
 	date_pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
 	time_pattern = re.compile(r'\d{2}:\d{2}:\d{2}')
@@ -54,4 +88,5 @@ def parseInfo(line):
 	return print(date, time, name)
 
 for line in sys.stdin:
-	parseInfo(line)
+	# parseInfo(line)
+	handleLine(line)
