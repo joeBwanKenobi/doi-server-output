3a79249ba558061cb608b1eb6764877c614f3836
Joe <josephbflanagan@gmail.com>
2018-11-19 14:57:29 -0500

    Added patterns for parsing, added handler action words.

--- servermon.py
+++ servermon.py
@@ -26,25 +26,40 @@ def handleLine(line):
 	"""
 	action_w = ""
 	find_words = ['connected','disconnected','joined','killed']
-	connected = re.compile(r'\sconnected')
-
-
+	
 
 	if ' connected' in line:
 		print('Handling Connection Line')
+		parseInfo(line)
 		print(line)
 	elif 'disconnected' in line:
 		print('Handling Disconnection Line')
+		parseInfo(line)
 		print(line)
 	elif 'joined' in line:
 		print('Handling Joined Line')
+		parseInfo(line)
 		print(line)
 	elif 'killed' in line:
 		print('Handling Killed Line')
+		parseInfo(line)
+		print(line)
+	elif ' say' in line:
+		print('Handling Global Say Line')
+		parseInfo(line)
+		print(line)
+	elif 'say_team' in line:
+		print('Handling Global Say Line')
+		parseInfo(line)
+		print(line)
+	elif 'triggered' in line:
+		print('Handling Triggered / Win Line')
+		parseInfo(line)
 		print(line)
 	else:
 		print('No Action Words found in line.')
-		print(line)
+		# parseInfo(line)
+		# print(line)
 
 
 def parseInfo(line):
@@ -54,38 +69,63 @@ def parseInfo(line):
 	an existing connection, update the server status, or output to 
 	console.
 	"""
+	ip = ""
 	date = ""
 	time = ""
-	connect_time = ""
-	disconnect_time = ""
 	uname = ""
 	steamID = ""
+	coords = ""
+	weapon = ""
+	message = ""
+	team_cur = ""
+	team_new = ""
+	team_join = ""
+	connect_time = ""
+	disconnect_time = ""
 
 	"""
 	Set patterns to search for
 	date: mm/dd/yyyy
 	time: hr:mm:ss
 	"""
+	ip_pattern = re.compile(r'\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}')
 	date_pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
 	time_pattern = re.compile(r'\d{2}:\d{2}:\d{2}')
-	name_pattern = re.compile(r'"([\w\s]+)')
+	uname_pattern = re.compile(r'"([\w\s]+)')
+	steamID_pattern = re.compile(r'STEAM_\d:\d:\d+')
+	coords_pattern = re.compile(r'(-?\d+\.\d+),\s(-?\d+\.\d+),\s(-?\d+\.\d+)')
+	team_cur_pattern = re.compile(r'(<#)(\w{2,15})')
+	team_join_pattern = re.compile(r'("#)(\w{2,15})')
 
 	"""
-	If patterns are NoneType, pass, else assign to variable
+	If patterns are NoneType, pass, otherwise assign to variable
 	"""
+	if ip_pattern.search(line) is not None:
+		ip = ip_pattern.search(line).group()
+
 	if date_pattern.search(line).group() is not None:
 		date = date_pattern.search(line).group()
-	else:
-		pass
+	
 	if time_pattern.search(line).group() is not None:
 		time = time_pattern.search(line).group()
-	else:
-		pass
-	if name_pattern.search(line) is not None:
-		name = name_pattern.search(line).group(1)
-	else:
-		pass
-	return print(date, time, name)
+	
+	if uname_pattern.search(line) is not None:
+		uname = uname_pattern.search(line).group(1)
+
+	if steamID_pattern.search(line) is not None:
+		steamID = steamID_pattern.search(line).group()
+
+	if coords_pattern.search(line) is not None:
+		coords = coords_pattern.search(line).group()
+
+	if team_cur_pattern.search(line) is not None:
+		team_curr = team_cur_pattern.search(line).group(2)
+
+	if team_join_pattern.search(line) is not None:
+		team_join = team_join_pattern.search(line).group(2)
+
+	
+	return print('\x1b[6;30;42m' + date, time, uname, ip, steamID, coords, team_cur, team_join + '\x1b[0m')
 
 for line in sys.stdin:
 	# parseInfo(line)
