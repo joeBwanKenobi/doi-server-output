072659566d207921e89d4cc6e0d81bcedf84f9c2
Joe <josephbflanagan@gmail.com>
2018-12-04 14:53:47 -0500

    server.py launches pty to run ./doi.sh or reads from file

--- servermon.py
+++ servermon.py
@@ -3,6 +3,9 @@ import re
 import datetime
 import urllib.request
 import ast
+import subprocess
+import pty
+import os
 
 class IterConnection(type):
 	def __iter__(cls):
@@ -37,7 +40,7 @@ class Connection(metaclass=IterConnection):
 
 	@property
 	def description(self):
-		return "L {}-{} > connection from address: {}\n{} {}\n{}, {}, {}\n{}".format(\
+		return "> {}-{} --> connection from address: {}\n{} {}\n{}, {}, {}\n{}".format(\
 			self.c_date, self.c_time, self.ip, self.uname, self.steam_id, self.location['city'], \
 			self.location['region'], self.location['country'], self.location['org'])
 
@@ -49,67 +52,53 @@ class Connection(metaclass=IterConnection):
 		return cls(c_date, c_time, steam_id, uname, ip, team_cur)
 
 
-# def get_location(ip):
-# 		print('IP ADDRESS IN get_location(): ' + ip)
-# 		with urllib.request.urlopen('https://ipinfo.io/{}'.format(ip)) as response:
-# 			loc = ast.literal_eval(response.read().decode('UTF-8'))
-# 		return loc
-
 
 def handle_line(line):
 	"""
 	Decides what to do with incoming data based on words in line,
-	connected = create connection object
+	connected, = create connection object
 	disconnected = destroy connection object, save to log file
 	killed = display line, update kills and deaths for connection objects
 	joined = display team change line, update server / team data
 	"""
-	action_w = ""
-	find_words = ['connected','disconnected','joined','killed']
 	
 
-	if ' connected' in line:
+	if ' connected,' in line:
 		# print('Handling Connection Line')
 		c1 = Connection.from_string(line)
 		print(c1.description)
-		
 	elif 'disconnected' in line:
-		pass
-		# print('Handling Disconnection Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Disconnection Line')
+		parse_info(line)
+		print(line)
 	elif 'joined' in line:
-		pass
-		# print('Handling Joined Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Joined Line')
+		parse_info(line)
+		print(line)
 	elif 'killed' in line:
-		pass
-		# print('Handling Killed Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Killed Line')
+		parse_info(line)
+		print(line)
 	elif ' say' in line:
-		pass
-		# print('Handling Global Say Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Global Say Line')
+		parse_info(line)
+		print(line)
 	elif 'say_team' in line:
-		pass
-		# print('Handling Global Say Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Global Say Line')
+		parse_info(line)
+		print(line)
 	elif 'triggered' in line:
-		pass
-		# print('Handling Triggered / Win Line')
-		# parse_info(line)
-		# print(line)
+		print('Handling Triggered / Win Line')
+		parse_info(line)
+		print(line)
 	else:
-		print('No Action Words found in line.')
+		print('--------->  No Action Words found in line.')
 		# parse_info(line)
-		# print(line)
+		print(line)
 
 
 def parse_info(line):
+	# import pdb; pdb.set_trace()
 	"""
 	Parses incoming strings from DOI Source Server, decides if the
 	string should be returned to create a connection object, update
@@ -128,12 +117,11 @@ def parse_info(line):
 	team_new = ""
 	team_join = ""
 
-	"""
-	Set patterns to search for
-	date: mm/dd/yyyy
-	time: hr:mm:ss
-	"""
-	ip_pattern = re.compile(r'\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3}')
+	"""Set patterns to search for"""
+
+	'''Find IPv4 address by searching for 3 groups of [0-9]. followed by one group of 3 [0-9] separated by whitespace on both sides
+	   this prevents assigning the first coordinate to ip variable'''
+	ip_pattern = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
 	date_pattern = re.compile(r'\d{2}/\d{2}/\d{4}')
 	time_pattern = re.compile(r'\d{2}:\d{2}:\d{2}')
 	uname_pattern = re.compile(r'"([\w\s]+)')
@@ -142,40 +130,69 @@ def parse_info(line):
 	team_cur_pattern = re.compile(r'(<#)(\w{2,15})')
 	team_join_pattern = re.compile(r'("#)(\w{2,15})')
 
-	"""
-	If patterns are NoneType, pass, otherwise assign to variable
-	"""
+	"""If patterns are NoneType, pass, otherwise assign to variable"""
 	if ip_pattern.search(line) is not None:
 		ip = ip_pattern.search(line).group()
+		print("IP ASSIGNED: {}".format(ip))
 
-	if date_pattern.search(line).group() is not None:
+	if date_pattern.search(line) is not None:
 		date = date_pattern.search(line).group()
+		print("DATE ASSIGNED: {}".format(date))
 	
-	if time_pattern.search(line).group() is not None:
+	if time_pattern.search(line) is not None:
 		time = time_pattern.search(line).group()
+		print("TIME ASSIGNED: {}".format(time))
 	
 	if uname_pattern.search(line) is not None:
 		uname = uname_pattern.search(line).group(1)
+		print("UNAME ASSIGNED: {}".format(uname))
 
 	if steam_id_pattern.search(line) is not None:
 		steam_id = steam_id_pattern.search(line).group()
-
+		print("STEAMID ASSIGNED: {}".format(steam_id))
 	if coords_pattern.search(line) is not None:
 		coords = coords_pattern.search(line).group()
+		print("COORDS ASSIGNED: {}".format(coords))
 
 	if team_cur_pattern.search(line) is not None:
 		team_curr = team_cur_pattern.search(line).group(2)
+		print("TEAM_CURR ASSIGNED: {}".format(team_curr))
 
 	if team_join_pattern.search(line) is not None:
 		team_join = team_join_pattern.search(line).group(2)
+		print("TEAM_JOIN ASSIGNED: {}".format(team_join))
 
 	
 	# print('\x1b[6;30;42m' + date, time, uname, ip, steam_id, coords, team_cur, team_join + '\x1b[0m')
 	return date, time, steam_id, uname, ip, team_cur, team_join, coords
 
-for line in sys.stdin:
-	# parse_info(line)
-	handle_line(line)
 
+
+''' Print Connection object instances '''
 # for c in Connection:
 # 	print(c)
+
+''' Open a pseudo terminal(pty) to run doi.sh script, output is buffered if piped
+	so a pty is required for real time srcds output
+'''
+
+# pdb.set_trace()
+# cmd = ['./doi.sh' ]
+# master, slave = pty.openpty()
+# p = subprocess.Popen(cmd, shell=True, stdin=slave, stdout=slave, stderr=slave, close_fds=True)
+# stdout = os.fdopen(master)
+# f = open("output.txt", "w")
+# try:
+# 	for line in stdout:
+# 		handle_line(line)
+# 		f.write(line)
+# except KeyboardInterrupt:
+# 	print("Keyboard Interrupt --> Exiting writing to logfile and exiting program....")
+
+
+file = open('logs/less-info.txt', 'r')
+try:
+	for line in file:
+		handle_line(line)
+except KeyboardInterrupt:
+	print("Keyboard Interrupt --> Exiting writing to logfile and exiting program....")
